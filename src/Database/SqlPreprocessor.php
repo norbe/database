a208f2ade8cd04578471463a2ee50c6cc4bc7cde
David Grudl <david@grudl.com>
2024-12-27 07:51:22 +0100

    SqlPreprocessor: added braces around where conditions

--- src/Database/SqlPreprocessor.php
+++ src/Database/SqlPreprocessor.php
@@ -342,7 +342,9 @@ class SqlPreprocessor
 			}
 		}
 
-		return '(' . implode(') ' . strtoupper($mode) . ' (', $res ?: [$default]) . ')';
+		return count($res) > 1
+			? '((' . implode(') ' . strtoupper($mode) . ' (', $res) . '))'
+			: '(' . ($res[0] ?? $default) . ')';
 	}
 
 
